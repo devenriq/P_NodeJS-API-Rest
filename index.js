8319401fd696b7cbe1d9663730aa84d9f7a3fa88
Enrique <octepc.97@gmail.com>
2022-07-21 17:16:38 -0500

    added some routes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("My App");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
+});
+
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
+});
+
+app.get('/productos', (req, res) => {
+  res.json({
+    name: 'producto1',
+    price: 1000,
+  });
+});
+
+app.get('/categories', (req, res) => {
+  res.json({
+    category1: 'clothes',
+    category2: 'jewerly',
+    category3: 'electronics',
+    category4: 'videogames',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Mi port ${port}`);
+});
