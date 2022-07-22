10390d8693faeb1af6c0dbb2b685686507b36cdb
Enrique <octepc.97@gmail.com>
2022-07-22 12:21:03 -0500

    added some other CRUD methods, like patch, create and delete

--- services/productService.js
+++ services/productService.js
@@ -16,15 +16,42 @@ class ProductService {
       });
     }
   }
-  create() {}
+  create(data) {
+    const newProduct = {
+      id: faker.datatype.uuid(),
+      ...data,
+    };
+
+    this.products.push(newProduct);
+    return newProduct;
+  }
   find() {
     return this.products;
   }
   findOne(id) {
     return this.products.find((item) => item.id === id);
   }
-  update() {}
-  delete() {}
+  update(id, changes) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    } else {
+      const product = this.products[index];
+      this.products[index] = {
+        ...product,
+        ...changes,
+      };
+      return this.products[index];
+    }
+  }
+  delete(id) {
+    const index = this.products.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.products.splice(index, 1);
+    return { id };
+  }
 }
 
 module.exports = ProductService;
