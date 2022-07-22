d9fed099c975e1d252d60a37df481c03697b696c
Enrique <octepc.97@gmail.com>
2022-07-21 20:46:11 -0500

    added services and routes to the api

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const routerApi = require('./routes');
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -10,47 +13,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/productos', (req, res) => {
-  res.json([
-    {
-      name: 'producto1',
-      price: 1000,
-    },
-    {
-      name: 'producto2',
-      price: 10000,
-    },
-  ]);
-});
-
-app.get('/products/:id/products', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'producto2',
-    price: 10000,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parámetros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
   console.log(`Mi port ${port}`);
